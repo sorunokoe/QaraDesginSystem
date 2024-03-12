8ea39a24c3180be8478d2de6333ead59ecdfd65e
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2024-03-12 13:36:11 +0100

    Add progressview

---
+++ Sources/QaraDesignSystem/ProgressView/SwiftUIView.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by SALGARA, YESKENDIR on 12.03.24.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    
+    var progress: CGFloat
+    var cornerRadius: CGFloat
+    var background: Color
+    var tint: Color
+    
+    var body: some View {
+        GeometryReader { geo in
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(background)
+                .overlay {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(tint)
+                        .frame(width: geo.size.width * progress)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+        }
+    }
+}
+
+#Preview {
+    ProgressView(progress: 0.8, cornerRadius: 16,
+                 background: .gray, tint: .blue)
+}
