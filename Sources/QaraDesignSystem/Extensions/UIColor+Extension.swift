cb26e888f34b736f301dc4a1fad96443dfc6d097
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2024-05-18 16:30:00 +0200

    Add extensions for navigationBat

---
+++ Sources/QaraDesignSystem/Extensions/UIColor+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by SALGARA, YESKENDIR on 18.05.24.
+//
+
+import UIKit
+
+extension UIColor {
+    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { rendererContext in
+            self.setFill()
+            rendererContext.fill(CGRect(origin: .zero, size: size))
+        }
+    }
+}
---
+++ Sources/QaraDesignSystem/Extensions/View+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by SALGARA, YESKENDIR on 18.05.24.
+//
+
+import SwiftUI
+import UIKit
+
+extension View {
+    @available(iOS 14, *)
+    func navigationBar(_ titleColor: Color, backgroundColor: Color) -> some View {
+        let uiColor = UIColor(titleColor)
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().setBackgroundImage(UIColor(backgroundColor).image(), for: .default)
+        UINavigationBar.appearance().isTranslucent = true
+        return self
+    }
+}
